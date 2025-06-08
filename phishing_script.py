8b3ae178718773679f834bede4d7832390c87a48
Virginie <virginielechene@proton.me>
2025-06-08 08:39:04 +0200

    Create phishing_script.py

---
+++ phishing_script.py
@@ -0,0 +1,18 @@
+import email
+from email import policy
+import sys
+
+def analyser_eml(fichier):
+with open(fichier, 'rb') as f:
+msg = email.message_from_binary_file(f, policy=policy.default)
+print("\nAnalyse de l'e-mail :\n")
+print("From :", msg.get('From'))
+print("Reply-To :", msg.get('Reply-To'))
+print("Return-Path:", msg.get('Return-Path'))
+print("Received :", msg.get_all('Received'))
+
+if __name__ == "__main__":
+if len(sys.argv) < 2:
+print("Usage : python phishing_checker.py email.eml")
+else:
+analyser_eml(sys.argv[1])
